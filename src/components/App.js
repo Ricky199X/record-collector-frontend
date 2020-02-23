301b401747ff8b4c56bdb96b55799e52a00264df
github username <github email address>
2020-02-23 13:05:34 -0500

    adds nav component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Nav from './Nav'
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Record Collector!</h1>
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+// functional component
+
+import React from 'react'
+
+const Nav = () => {
+   return (
+      <nav>
+         <div className="nav-wrapper container">
+            <a href="" className="brand-logo">Record Collector</a>
+         </div>
+      </nav>
+   )
+}
+
+export default Nav
